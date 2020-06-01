47e5d17b7d4922276eb4a38bbeb6ad3da59bf10d
alecvipa <hebrit_626@hotmail.com>
2020-06-01 10:35:26 -0500

    version sin estilo y con dinamismo en tienda. hace falta scroll spy

---
+++ public/store.js
@@ -0,0 +1,220 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+$(".zipAlert, .zipAlert2, .formdiv, .purchDiv").hide();
+$(".unit1, .unit2, .unit3, .unit4, .unit5, .unit6").hide();
+
+// Función para botones de imágen
+$(".btn1first").on("click", function() {
+    $('.imgPre').css("background-image", "url(Images/Album1.png)");
+});
+$(".btn1second").on("click", function() {
+    $('.imgPre').css("background-image", "url(Images/Album2.png)");
+});
+$(".btn1third").on("click", function() {
+    $('.imgPre').css("background-image", "url(Images/Album3.png)");
+});
+$(".btn2first").on("click", function() {
+    $('.imgPre').css("background-image", "url(Images/Album1.png)");
+});
+$(".btn2second").on("click", function() {
+    $('.imgPre').css("background-image", "url(Images/Album2.png)");
+});
+$(".btn2third").on("click", function() {
+    $('.imgPre').css("background-image", "url(Images/Album3.png)");
+});
+$(".btn3first").on("click", function() {
+    $('.imgPre').css("background-image", "url(Images/Album1.png)");
+});
+$(".btn3second").on("click", function() {
+    $('.imgPre').css("background-image", "url(Images/Album2.png)");
+});
+$(".btn3third").on("click", function() {
+    $('.imgPre').css("background-image", "url(Images/Album3.png)");
+});
+$(".btn4first").on("click", function() {
+    $('.imgPre').css("background-image", "url(Images/Album1.png)");
+});
+$(".btn4second").on("click", function() {
+    $('.imgPre').css("background-image", "url(Images/Album2.png)");
+});
+$(".btn4third").on("click", function() {
+    $('.imgPre').css("background-image", "url(Images/Album3.png)");
+});
+// FUNCION PARA BOTONES DE DETALLES
+$("#detail1").on("click", function(){
+    $(".unit1").show();
+    $(".unit2, .unit3, .unit4, .unit5, .unit6, #experienciasCards").hide();
+});
+$("#detail2").on("click", function(){
+    $(".unit2").show();
+    $(".unit1, .unit3, .unit4, .unit5, .unit6, #experienciasCards").hide();
+});
+$("#detail3").on("click", function(){
+    $(".unit3").show();
+    $(".unit1, .unit2, .unit4, .unit5, .unit6, #experienciasCards").hide();
+});
+$("#detail4").on("click", function(){
+    $(".unit4").show();
+    $(".unit1, .unit2, .unit3, .unit5, .unit6, #experienciasCards").hide();
+});
+$("#detail5").on("click", function(){
+    $(".unit5").show();
+    $(".unit1, .unit2, .unit3, .unit4, .unit6, #experienciasCards").hide();
+});
+$("#detail6").on("click", function(){
+    $(".unit6").show();
+    $(".unit1, .unit2, .unit3, .unit4, .unit5, #experienciasCards").hide();
+});
+$(".btnRegresar").on("click", function(){
+    $("#experienciasCards").show();
+    $(".unit1, .unit2, .unit3, .unit4, .unit5, .unit6").hide();
+});
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+var stripeHandler = StripeCheckout.configure({
+    key: stripePublicKey,
+    locale: 'en',
+    token: function (token) {
+        var items = []
+        var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+        var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+        for (var i = 0; i < cartRows.length; i++) {
+            var cartRow = cartRows[i]
+            var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+            var quantity = quantityElement.value
+            var id = cartRow.dataset.itemId
+            items.push({
+                id: id,
+                quantity: quantity
+            })
+        }
+
+        fetch('/purchase', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                stripeTokenId: token.id,
+                items: items
+            })
+        }).then(function (res) {
+            return res.json()
+        }).then(function (data) {
+            alert(data.message)
+            var cartItems = document.getElementsByClassName('cart-items')[0]
+            while (cartItems.hasChildNodes()) {
+                cartItems.removeChild(cartItems.firstChild)
+            }
+            updateCartTotal()
+        }).catch(function (error) {
+            console.error(error)
+        })
+    }
+})
+
+function purchaseClicked() {
+    var priceElement = document.getElementsByClassName('cart-total-price')[0]
+    var price = parseFloat(priceElement.innerText.replace('$', '')) * 100
+    stripeHandler.open({
+        amount: price
+    })
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    var id = shopItem.dataset.itemId
+    addItemToCart(title, price, imageSrc, id)
+    updateCartTotal()
+}
+var number= 1;
+
+function addItemToCart(title, price, imageSrc, id) {
+    number++;
+    var cartRow = document.createElement('div');
+    cartRow.classList.add('cart-row');
+    cartRow.dataset.itemId = id;
+    var cartItems = document.getElementsByClassName('cart-items')[0];
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title');
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('Esta experiencia ya ha sido añadida al carrito');
+            return;
+        }
+    }
+    
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <input type="hidden" value="${title}" name="title">
+            <span class="cart-item-title" >${title}</span>
+        </div>
+        <span class="cart-price cart-column" name="price">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1" name="quantity">
+            <button class="btn btn-danger" type="button">ELIMINAR</button>
+        </div>`
+    cartRow.innerHTML = cartRowContents;
+    cartItems.append(cartRow);
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem);
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged);
+   
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
---
+++ server.js
@@ -0,0 +1,161 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load()
+}
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+const email = process.env.email;
+const superSecretPwd = process.env.superSecretPwd;
+
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const stripe = require('stripe')(stripeSecretKey);
+const nodemailer = require("nodemailer");
+var PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.static('public'))
+
+
+
+
+
+app.get('/', function(req, res){
+  fs.readFile('items.json', function (error, data) {
+    if (error) {
+      res.status(500).end()
+    } else {
+      res.render('store.ejs', {
+        stripePublicKey: stripePublicKey,
+        items: JSON.parse(data)
+      });
+      console.log("items.json");
+    }
+  })
+})
+
+app.get('/store', function (req, res) {
+  fs.readFile('items.json', function (error, data) {
+    if (error) {
+      res.status(500).end()
+    } else {
+      res.render('store.ejs', {
+        stripePublicKey: stripePublicKey,
+        items: JSON.parse(data)
+      });
+    }
+  });
+});
+
+app.get('/privacy', function (req, res) {
+  fs.readFile('items.json', function (error, data) {
+    if (error) {
+      res.status(500).end()
+    } else {
+      res.render('privacy.ejs', {
+        stripePublicKey: stripePublicKey,
+        items: JSON.parse(data)
+      })
+    }
+  })
+})
+
+app.post('/purchase', function (req, res) {
+  fs.readFile('items.json', function (error, data) {
+    if (error) {
+      res.status(500).end()
+    } else {
+      const itemsJson = JSON.parse(data)
+      const itemsArray = itemsJson.music.concat(itemsJson.merch)
+      let total = 0
+      req.body.items.forEach(function (item) {
+        const itemJson = itemsArray.find(function (i) {
+          return i.id == item.id
+        })
+        total = total + itemJson.price * item.quantity
+      })
+
+      stripe.charges.create({
+        amount: total,
+        source: req.body.stripeTokenId,
+        currency: 'mxn'
+      }).then(function () {
+        console.log('Cargo exitoso')
+        res.json({ message: 'Artículos comprados exitosamente' })
+      }).catch(function () {
+        console.log('Cobro fallido')
+        res.status(500).end()
+      })
+    }
+  })
+})
+
+app.get("/api/:zips?", function (req, res) {
+  var zipCodes = [
+    {
+        name: "Códigos Postales Disponibles",
+        disponibles: ["55140","52930","52938"],
+    },
+  ];
+
+  var chosen = req.params.zips;
+  console.log(chosen);
+  console.log(zipCodes[0].disponibles[0]);
+
+  if (chosen) {
+
+      for (var i = 0; i < zipCodes.length; i++) {
+          if (chosen === zipCodes[0].disponibles[0] || chosen === zipCodes[0].disponibles[1] || chosen === zipCodes[0].disponibles[2] ) {
+              return res.json(zipCodes[i]);
+          }
+      }
+
+      return res.send("Código postal sin cobertura");
+  }
+  else {
+      res.json(otherModels);
+  }
+
+});
+
+
+app.post("/ajax/email", function (request, response) {
+  const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      secure: false,
+      port: 25,
+      auth: {
+          user: email,
+          pass: superSecretPwd
+      },
+      tls: {
+          rejectUnauthorized: false
+      }
+  });
+  console.log(request.body);
+
+  var textBody = `FROM: ${request.body.name}; EMAIL: ${request.body.email}; NUMBER: ${request.body.number}; MESSAGE: ${request.body.message}; DATE: ${request.body.date}; HOUR:${request.body.hour}; CHECKBOX:${request.body.checkbox}`;
+  var htmlBody = `<h2>Solicitud de pedido</h2><p>Nombre: ${request.body.name}</p><p> Correo electrónico: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Telefono de contacto: ${request.body.number}</p><p>Mensaje: ${request.body.message}</p><p>Día de entrega: ${request.body.date}</p><p>Hora de entrega: ${request.body.hour}</p><p>Quieren novedades: ${request.body.checkbox}</p><p>Pedido: ${request.body.pedido}</p>`;
+  var mail = {
+      from: '"Team: Dilan" <dilan@gmail.com>',
+  to: 'hebrit_626@hotmail.com',
+  subject: '¡Alguien te ha hecho un pedido!',
+  html:htmlBody
+  };
+  transporter.sendMail(mail,function(err, info){
+      if (err) {
+          return console.log(err);
+      } else {
+          console.log("message sent!");
+          console.log(request.body);
+      };
+  });
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
